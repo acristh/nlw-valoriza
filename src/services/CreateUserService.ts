0b518db59a8b7213cefcfa7567a1e253edd05db6
Andre Cristiano <andre.cristiano1975@gmail.com>
2021-07-01 20:54:13 -0300

    feat: create service CreateUserService to create a new user

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { UserRepository } from '../repositories/UserRepository';
+
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+class CreateUserService {
+    async execute({ name, email, admin }: IUserRequest) {
+        const userRepository = new UserRepository();
+
+        if (!email) {
+            throw new Error("Email incorrect");
+        }
+
+        const userAlreadyExists = await userRepository.findOne({
+            email
+        });
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const user = userRepository.create({
+            name,
+            email,
+            admin,
+        });
+
+        await userRepository.save(user);
+
+        return user;
+        
+    }
+}
